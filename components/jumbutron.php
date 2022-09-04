1bf455e36c04269f1558c6b650d21b0e20ca208a
Momen-Alshouha <momenalshouha@outlook.com>
2022-09-04 23:36:04 +0300

    done

---
+++ components/jumbutron.php
@@ -0,0 +1,5 @@
+<div style="background-color: #f7f7f7;padding:20px" class="jumbotron container ">
+  <h1 class="display-4">Hello, world!</h1><br />
+  <p class="display-6">This is a simple php project, CRUD (Create, Read, Update, Delete) operation using ajax (without page refresh), Bootstrap Popup Modal.</p>
+  <hr class="my-4">
+</div>
